7b9bdbf8542a64dbaf8fe1738b2cceef728ca446
Pranay Agarwal <pranayag15@gmail.com>
2019-07-13 14:03:17 +0530

    added server.js

---
+++ build/precache-manifest.84130c2592e47e6230a7dadf089a8f86.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79c57562bfbe151cd61201f01428048d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f6240a57c112c4c23c31",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "1bd579e3135010f42fe8",
+    "url": "/static/js/2.942e41f5.chunk.js"
+  },
+  {
+    "revision": "f6240a57c112c4c23c31",
+    "url": "/static/js/main.fb8de12b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
